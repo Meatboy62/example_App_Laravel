74673652d94a609578645ec844fcbac68a3a432b
Meatboy62 <mariogonzalez6276@gmail.com>
2023-03-17 14:32:10 -0600

    Final del segundo dia

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    private $posts = [
+        1 => [
+            'title' => 'Intro to Laravel',
+            'content' => 'This is a short intro to Laravel'
+        ],
+        2 => [
+            'title' => 'Intro to PHP',
+            'content' => 'This is a short intro to PHP'
+        ],
+        3 => [
+            'title' => 'Intro to risk of rain',
+            'content' => 'Obliderate yourself'
+        ]
+    ];
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('posts.index', ['posts' => $this->posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        abort_if(!isset($this->posts[$id]), 404);
+        return view('posts.show', ['post' => $this->posts[$id]]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,48 +25,52 @@ use Illuminate\Support\Facades\Route;
 // })->name('home.contact');
 
 
-Route::view('/', 'welcome')
-    ->name('home.welcome');
+Route::get('/', [HomeController::class, 'home'])
+->name('home.welcome');
 
-Route::view('/contact', 'contact.index')
-    ->name('home.contact');
-
-
-$posts = [
-    1 => [
-        'title' => 'Intro to Laravel',
-        'content' => 'This is a short intro to Laravel'
-    ],
-    2 => [
-        'title' => 'Intro to PHP',
-        'content' => 'This is a short intro to PHP'
-    ],
-    3 => [
-        'title' => 'Intro to risk of rain',
-        'content' => 'Obliderate yourself'
-    ]
-];
+Route::get('/contact', [HomeController::class, 'contact'])
+->name('home.contact');
 
 
-Route::get('/posts', function () use ($posts) {
-    return view('posts.index', ['posts' => $posts]);
-});
-Route::get('/posts/{$id}', function ($id) use ($posts) {
-    abort_if(!isset($posts[$id]), 404);
-    return view('posts.show', ['post' => $posts[$id]]);
-});
-
-Route::get('/fun/responses', function() use($posts) {
-    return response($posts, 201)
-        ->header('Content-Type', 'application/json')
-        ->cookie('MY_COOKIE', 'Piotr Jura', 3600);
-});
-
-Route::get('/fun/json', function () use ($posts) {
-    return response()->json($posts);
-});
 
 
-Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
-    return 'Past from ' . $daysAgo . ' days ago';
-})->name('post.show');
+
+Route::resource('posts',PostController::class);
+
+// Route::get('/posts', function () use ($posts) {
+//     //dd(request()->all());
+//     return view('posts.index', ['posts' => $posts]);
+// });
+
+
+
+
+// Route::get('/posts/{$id}', function ($id) use ($posts) {
+//     abort_if(!isset($posts[$id]), 404);
+//     return view('posts.show', ['post' => $posts[$id]]);
+// });
+
+
+// Route::get('/fun/responses', function() use($posts) {
+//     return response($posts, 201)
+//         ->header('Content-Type', 'application/json')
+//         ->cookie('MY_COOKIE', 'Piotr Jura', 3600);
+// });
+
+// Route::get('/fun/json', function () use ($posts) {
+//     return response()->json($posts);
+// });
+
+
+// Route::get('/fun/download', function() use($posts){
+//  return response() ->download(public_path('/Meatboy.png'),'Metaboy.png');
+// });
+
+
+
+
+// Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
+//     return 'Past from ' . $daysAgo . ' days ago';
+// })->name('post.recent.index')->middleware('auth');
+
+
